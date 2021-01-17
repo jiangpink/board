e278524d26af6348690e0dcd749f5435a39b6df4
jiangpink <1337866160@qq.com>
2021-01-17 22:56:52 +0800

    data-test

--- data-test/data-test.js
+++ data-test/data-test.js
@@ -6,7 +6,8 @@ getRequest.send()
 getRequest.onreadystatechange = function() {
   if (getRequest.readyState == 4) { 
     if (getRequest.status == 200) { 
-      document.getElementById('test').innerHTML=getRequest.responseText
+      document.getElementById('test').innerHTML=getRequest.responseText;
+      document.getElementById('test').innerHTML=JSON.parse(getRequest.responseText);
     }
   }
 }
